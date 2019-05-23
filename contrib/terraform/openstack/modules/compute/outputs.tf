16df290fd54c8c7a5f3e72d12c48a5909a78b587
Marco Enrico Piras <kikkomep@crs4.it>
2019-05-23 11:42:25 +0200

    Fix ip output

--- contrib/terraform/openstack/modules/compute/outputs.tf
+++ contrib/terraform/openstack/modules/compute/outputs.tf
@@ -3,5 +3,5 @@ output "k8s_master_ext_net_ips" {
 }
 
 output "k8s_node_ext_net_ips" {
-  value = ["${openstack_compute_instance_v2.k8s_node_ext_net.*.network.0.fixed_ip_v4}"]
+  value = ["${openstack_compute_instance_v2.k8s_node_ext_net.*.network.0.fixed_ip_v4}", "${openstack_compute_instance_v2.k8s_data_node_ext_net.*.network.0.fixed_ip_v4}"]
 }
